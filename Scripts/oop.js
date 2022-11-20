5988e53e33a530ef1640458e7deefa12cb4cd7ad
Milodrag <Ali.the.iraqer@gmail.com>
2022-11-20 21:01:11 +0100

    FerdigProdukt

--- Scripts/oop.js
+++ Scripts/oop.js
@@ -1,40 +1,35 @@
 let reflection = {};
 
-//http://ejohn.org/blog/simple-javascript-inheritance/
 (function(){
 	let initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
 
-	// The base Class implementation (does nothing)
 	this.Class = function(){};
    
-	// Create a new Class that inherits from this class
+	// Opprett en ny klasse som arver fra denne klassen
 	Class.extend = function(prop, ref_name) {
 		if(ref_name)
 			reflection[ref_name] = Class;
 			
 		let _super = this.prototype;
 
-		// Instantiate a base class (but only create the instance,
-		// don't run the init constructor)
+		// Instantier en basisklasse (men opprett bare forekomsten, ikke kjør init-konstruktøren)
 		initializing = true;
 		let prototype = new this();
 		initializing = false;
 		 
-		// Copy the properties over onto the new prototype
+		// Kopier egenskapene over på den nye prototypen
 		for (let name in prop) {
-		// Check if we're overwriting an existing function
+		// Sjekker om vi overskriver en eksisterende funksjon
 		prototype[name] = typeof prop[name] == "function" && 
 			typeof _super[name] == "function" && fnTest.test(prop[name]) ?
 			(function(name, fn) {
 				return function() {
 					let tmp = this._super;
 
-					// Add a new ._super() method that is the same method
-					// but on the super-class
+					// Legg til en ny ._super()-metode som er samme metode, men på superklassen
 					this._super = _super[name];
 
-					// The method only need to be bound temporarily, so we
-					// remove it when we're done executing
+					// Metoden må bare bindes midlertidig, så vi fjerner den når vi er ferdige med å kjøre
 					let ret = fn.apply(this, arguments);        
 					this._super = tmp;
 
@@ -44,20 +39,21 @@ let reflection = {};
 			prop[name];
 		}
 		 
-		// The dummy class constructor
+			// Dummy-klassekonstruktøren
 		function Class() {
-			// All construction is actually done in the init method
+
+			// All konstruksjon gjøres faktisk i init-metoden
 			if ( !initializing && this.init )
 				this.init.apply(this, arguments);
 		}
 		 
-		// Populate our constructed prototype object
+		// Fyll ut vårt konstruerte prototypeobjekt
 		Class.prototype = prototype;
 		 
-		// Enforce the constructor to be what we expect
+		// Tving konstruktøren til å være det vi forventer
 		Class.prototype.constructor = Class;
 
-		// And make this class extendable
+		// Og gjør denne klassen utvidbar
 		Class.extend = arguments.callee;
 		 
 		return Class;
